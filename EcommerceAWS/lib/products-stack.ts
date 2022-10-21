a8afee75ba2c8a046b00de112a01f6066a288582
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-10-21 17:14:32 -0300

    :sparkles: feat: Criando a primeira função Lambda - Leitura de produtos

---
+++ EcommerceAWS/lib/products-stack.ts
@@ -0,0 +1,27 @@
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs"
+import * as cdk from "aws-cdk-lib"
+import { Construct } from "constructs"
+
+export class ProductsStack extends cdk.Stack {
+    
+    //Leitura de produtos
+    readonly productsFetchHandler: lambdaNodeJs.NodejsFunction
+
+    constructor(scope: Construct, id:string, props?: cdk.StackProps){
+        super(scope, id, props)
+
+        this.productsFetchHandler = new lambdaNodeJs.NodejsFunction(this, 
+            "productsFetchFunction", {
+                FunctionName: "productsFetchFunction",
+                entry: "lambda/products/productsFetchFunction.ts",
+                handler: "handler",
+                memorySize: 128,
+                timeout: cdk.Duration.seconds(5),
+                bundling: {
+                   minify:  true,
+                   sourceMap: false
+                },
+        })
+    }
+}
