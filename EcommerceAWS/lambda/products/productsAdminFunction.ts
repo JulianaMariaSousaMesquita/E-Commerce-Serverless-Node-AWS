75e396bba8eda5ea991e8ff24d4222ea51ff49cb
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-10-25 14:33:16 -0300

    :sparkles: feat: Atualizando função de pesquisa de produtos e Adicionando função de administração de produtos com POST,PUT e DELETE

---
+++ EcommerceAWS/lambda/products/productsAdminFunction.ts
@@ -0,0 +1,37 @@
+import { Method } from "aws-cdk-lib/aws-apigateway"
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context }from "aws-lambda"
+
+export async function handler(event: APIGatewayProxyEvent, 
+    context: Context): Promise <APIGatewayProxyResult> {
+
+        const lambdaRequestId = context.awsRequestId
+        const apiResquestId = event.requestContext.requestId 
+        console.log(`API Gateway RequestId: ${apiResquestId} - lambda RequestId: ${lambdaRequestId}`)
+        
+        if(event.resource === "/products"){
+            console.log("POST /products")
+            return{
+                statusCode:201,
+                body:"POST /products"
+            }
+        }else if (event.resource === "products/{id}"){
+            const productsId = event.pathParameters!.id as string
+            if(event.httpMethod === "PUT"){
+                console.log(`PUT /products${productsId}`)
+            return{
+                statusCode:200,
+                body:`PUT /products${productsId}`
+            }
+            }else if (event.httpMethod === "DELETE"){
+                console.log(`DELETE /products${productsId}`)
+            return{
+                statusCode:200,
+                body:`DELETE /products${productsId}`
+            }
+            }
+        }
+        return{
+            statusCode: 400,
+            body:"Bad request"
+        }
+    }
