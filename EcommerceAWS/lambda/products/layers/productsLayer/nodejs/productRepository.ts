71347a066539a2c1cd8098713936d6cb85d78343
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-10-25 20:35:25 -0300

    :sparkles: feat: Criando repositório de produtos

--- EcommerceAWS/lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ EcommerceAWS/lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -1,7 +1,21 @@
+import { DocumentClient} from "aws-sdk/clients/dynamodb"
+import {v4 as uuid} from "uuid"
 export interface Product {
     id: string;
     productName: string;
     code: string;
     price: number;
     model: string;
+}
+
+export class ProductRepository {
+    //Cliente que vai dar acesso ao dynamodb
+    private ddbClient: DocumentClient
+    //Nome da tabela
+    private ddbProducts: string
+    
+    constructor (ddbClient: DocumentClient, ddbProducts: string){
+        this.ddbClient = ddbClient
+        this.ddbProducts = ddbProducts        
+    } 
 }
