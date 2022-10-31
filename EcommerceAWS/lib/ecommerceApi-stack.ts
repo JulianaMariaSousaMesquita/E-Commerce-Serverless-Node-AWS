0dbce8a5906a0e62f20eb404f699de5feb06b387
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-10-30 23:46:49 -0300

    :sparkles: feat : Criando user pool do cliente do E-commerce

--- EcommerceAWS/lib/ecommerceApi-stack.ts
+++ EcommerceAWS/lib/ecommerceApi-stack.ts
@@ -2,6 +2,8 @@ import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs"
 import * as cdk from "aws-cdk-lib"
 import * as apigateway from "aws-cdk-lib/aws-apigateway"
 import * as cwlogs from "aws-cdk-lib/aws-logs"
+import * as cognito from "aws-cdk-lib/aws-cognito"
+import * as lambda from "aws-cdk-lib/aws-lambda"
 import { Construct } from "constructs"
 
 interface ECommerceStackProps extends cdk.StackProps {
@@ -10,6 +12,9 @@ interface ECommerceStackProps extends cdk.StackProps {
 }
 
 export class ECommerceApiStack extends cdk.Stack{
+    private productAuthorizer: apigateway.CognitoUserPoolsAuthorizer
+    private customerPool: cognito.UserPool
+    private adminPool: cognito.UserPool
 
     constructor(scope: Construct, id:string, props: ECommerceStackProps){
         super(scope, id, props)
@@ -53,5 +58,60 @@ export class ECommerceApiStack extends cdk.Stack{
 
         // DELETE /products/{id}
         productIdResource.addMethod("DELETE", productsAdminIntegration)
+
+        this.createCognitoAuth()
+    }
+
+    private createCognitoAuth(){
+        //Cognito customer UserPool (Usuários que podem acessar plataforma para pesquisar produtos)
+        this.customerPool = new cognito.UserPool(this, "CustomerPool", {
+            userPoolName: "CustomerPool",
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            selfSignUpEnabled: true,
+            autoVerify: {
+                email: true,
+                phone: false
+            },
+            userVerification: {
+                emailSubject: "Verifique o seu e-mail para acessar o serviço de E-commerce!",
+                emailBody: "Obrigada por se registrar! Seu codigo de verificação : {####}",
+                emailStyle: cognito.VerificationEmailStyle.CODE
+            },
+            signInAliases: {
+                username: false,
+                email: true
+            },
+            standardAttributes: {
+                fullname: {
+                    required: true,
+                    mutable: false
+                }                
+            },
+            passwordPolicy: {
+                minLength: 8,
+                requireLowercase: true,
+                requireUppercase: true,
+                requireDigits: true,
+                requireSymbols: true,
+                tempPasswordValidity: cdk.Duration.days(3)
+            },
+            accountRecovery: cognito.AccountRecovery.EMAIL_ONLY
+        })
+
+        this.customerPool.addDomain("CustomerDomain", {
+            cognitoDomain: {
+                domainPrefix: "julianamesquita-customer-service"
+            }
+        })
+
+        const customerWebScope = new cognito.ResourceServerScope({
+            scopeName: "web",
+            scopeDescription: "Customer Web Operation"
+        })
+
+        const customerMobileScope = new cognito.ResourceServerScope({
+            scopeName: "mobile",
+            scopeDescription: "Customer mobile Operation"
+        })
     }
 }
