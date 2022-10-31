d5f7fd235379704aa38ce3b8357f589ccbf6a311
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-10-30 21:53:27 -0300

    :sparkles: feat : Criação da autenticação com o  token

--- EcommerceAWS/app.js
+++ EcommerceAWS/app.js
@@ -16,6 +16,36 @@ app.get('/', (req, res) => {
     res.status(200).json({msg: "Bem vindo - API Usuario"})
 })
 
+//Rota privada
+app.get("/user/:id", checkToken, async (req, res) => {
+    const id = req.params.id
+
+    //Checar se usuário existe
+    const user = await User.findById(id, '-password')
+    if(!user){
+        return res.status(404).json({msg: 'Usuário não encontrado!'})
+    }
+    res.status(200).json({ user })
+})
+
+//Função para verificar o Token
+function checkToken (req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if(!token){
+        return res.status(401).json({msg:'Acesso negado!'})
+    }
+
+    try{
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+        next()
+    }catch(error){
+        res.status(400).json({msg: "Token inválido!"})
+    }
+}
+
 //Registrar usuario
 app.post('/auth/register', async(req, res) => {
     const { name, email, password, confirmpassword } = req.body
